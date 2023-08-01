a984c89242abc7446112bde963344e6790458e7d
Sami Khokher <112092098+Codeit88@users.noreply.github.com>
2023-08-01 14:02:13 +0500

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,30 @@
+const app=Vue.createApp({
+   data(){
+      return{
+         template:'I am hunter',
+         noir:'Good to go',
+         age: 15,
+      };
+   },
+   methods:{
+      incAge(){
+         return this.age++;
+      },
+      toggleNoir(){
+       this.showNoir=!this.showNoir;
+      }
+   }
+ });
+ app.mount('#app');
+
+
+ ---/////////////////////html
+
+<div v-if="showNoir">
+    <p>{{template}} - {{noir}} - {{age}}</p>
+</div>
+
+<button v-on:click="toggleNoir">
+<div v-if=showNoir>Toggle Details</div>
+<div v-else=!showNoir>Toggle Details</div>
+</button>
