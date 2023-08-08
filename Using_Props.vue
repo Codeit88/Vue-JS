06304db33bc3a0308770e2ccbfd8bc70a39a8108
Sami Khokher <112092098+Codeit88@users.noreply.github.com>
2023-08-08 03:37:01 -0700

    Add files via upload

---
+++ Using_Props.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>
+    You are Awesome
+  </h1>
+  <ModalWar />
+</template>
+
+<script>
+import ModalWar from './components/ModalWar.vue'
+
+export default {
+  components: {
+    ModalWar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+
+---//////////////////////////////////////////modalwar
+
+<template>
+    <DIV id="GR">
+        <h1>
+            chuphja
+        </h1>
+    </DIV>
+</template>
+
+<script>
+export default {
+    name: "ModalWar"
+}
+</script>
