62f073ae910e022f5c8e78d708a391baa305187d
Sami Khokher <112092098+Codeit88@users.noreply.github.com>
2023-08-08 03:38:17 -0700

    Add files via upload

--- Using_Props.vue
+++ Using_Props.vue
@@ -1,45 +1,31 @@
-<template>
-  <h1>
-    You are Awesome
-  </h1>
-  <ModalWar />
-</template>
-
 <script>
 import ModalWar from './components/ModalWar.vue'
 
 export default {
   components: {
     ModalWar
+  },
+  data () {
+    return {
+      value: 'Value passed from parent'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-
-
-
----//////////////////////////////////////////modalwar
-
+///////////////////////
+modalwar
 <template>
     <DIV id="GR">
         <h1>
-            chuphja
+            {{value}} is very Uncertain
         </h1>
     </DIV>
 </template>
 
 <script>
 export default {
-    name: "ModalWar"
+    name: "ModalWar",
+    props: ['value'],
 }
 </script>
